9c6b8cdf709fad33adbe9bebe2f4842718fa313d
shanfeng606 <57214519+shanfeng606@users.noreply.github.com>
2020-06-01 15:52:14 +0800

    Add files via upload
    番茄时钟

---
+++ pages/index/index.js
@@ -0,0 +1,185 @@
+//index.js
+//获取应用实例
+const app = getApp()
+const util = require('../../utils/util.js')
+
+Page({
+  data: {
+    time:'5',
+    rate:'',
+    clockShow:false,
+    mTime:0,
+    timer:null, //把计时器暴露出来，供其他函数调用
+    timeStr:'05:00',
+    cateArr:[
+      {
+        icon:'work',
+        text:'工作'
+      },{
+        icon:'study',
+        text:'学习'
+      },{
+        icon:'think',
+        text:'思考'
+      },{
+        icon:'write',
+        text:'写作'
+      },{
+        icon:'sport',
+        text:'运动'
+      },{
+        icon:'read',
+        text:'阅读'
+      }
+    ],
+    cateActive:'0',
+    okShow:false,
+    pauseShow:true,
+    continueCancelShow:false
+  },
+  //事件处理函数
+  bindViewTap: function() {
+    wx.navigateTo({
+      url: '../logs/logs'
+    })
+  },
+  onLoad: function () {
+    //系统设备信息
+    var res=wx.getSystemInfoSync();
+    var rate=750 / res.windowWidth;
+    this.setData({
+      rate:rate
+    })
+    
+  },
+  getUserInfo: function(e) {
+    console.log(e)
+    app.globalData.userInfo = e.detail.userInfo
+    this.setData({
+      userInfo: e.detail.userInfo,
+      hasUserInfo: true
+    })
+  },
+  slideChange:function(e){
+    this.setData({
+     time:e.detail.value
+    })
+  },
+  clickCate:function(e){
+    this.setData({
+      cateActive:e.currentTarget.dataset.index
+    })
+  },
+  // 开始计时按钮 
+  start:function(){
+    this.setData({
+      clockShow:true,
+      timeStr:parseInt(this.data.time)>=10 ? this.data.time+':00':'0'+ this.data.time+':00'
+    })
+    this.drawBg();
+    this.drawActive();
+    
+  },
+
+  // 返回按钮 
+  clock:function(){
+    this.setData({
+      clockShow:false,
+      timeStr:'05:00'
+    }) 
+  },
+  // 动圆函数 
+  drawActive:function(){
+    var _this=this;
+    var currentTime=this.data.time*60*1000-_this.data.mTime
+    var timer=setInterval(() => {
+      currentTime=currentTime-100  //倒计时文字
+      
+      _this.setData({
+        mTime:_this.data.mTime+100   //每100毫秒执行一次，每次时间加100毫秒，并绘图
+      })
+      var step=_this.data.mTime/(_this.data.time*60*1000)*2*Math.PI+1.5*Math.PI
+      
+      if(step<3.5*Math.PI){
+
+        if(currentTime % 1000){
+          var time_all=currentTime / 1000;  //获得倒计时文字总秒数
+          var time_m=(parseInt(time_all / 60)>=10)?parseInt(time_all / 60):('0'+parseInt(time_all / 60))                 //获得倒计时文字分钟
+          var time_s=(parseInt(time_all % 60)>10)?parseInt(time_all % 60):'0'+parseInt(time_all % 60);                 //获得剩余倒计时秒数
+          _this.setData({
+            timeStr:time_m+':'+time_s
+          })
+        }
+
+        var lineWidth=6/_this.data.rate; //px
+        var ctx=wx.createCanvasContext('progress-active');
+        ctx.setLineWidth(lineWidth);
+        ctx.setStrokeStyle('#fff');
+        ctx.setLineCap('round');
+        ctx.beginPath();
+        ctx.arc(400/_this.data.rate/2,400/_this.data.rate/2,400/_this.data.rate/2-2*lineWidth,1.5*Math.PI,step);
+        ctx.stroke();
+        ctx.draw();
+        
+      }else{
+        // 将完成的数据记录到日志
+        var logs=wx.getStorageSync('logs')||[];
+        logs.unshift({
+          date:util.formatTime(new Date),
+          cate:_this.data.cateActive,
+          time:_this.data.time
+        });
+        // console.log(logs); 
+        wx.setStorageSync('logs', logs)//把数据加到缓存
+
+        _this.setData({
+          timeStr:"00:00",
+          okShow:true,
+          pauseShow:false,
+          continueCancelShow:false
+        })
+        clearInterval(timer);
+      }
+    }, 100);  
+    this.setData({
+      timer:timer
+    })
+  },
+  // 静圆函数 
+  drawBg:function(){
+    var lineWidth=6/this.data.rate; //px
+    var ctx=wx.createCanvasContext('progress-bg');
+    ctx.setLineWidth(lineWidth);
+    ctx.setStrokeStyle('#000');
+    ctx.setLineCap('round');
+    ctx.beginPath();
+    ctx.arc(400/this.data.rate/2,400/this.data.rate/2,400/this.data.rate/2-2*lineWidth,0,2*Math.PI);
+    ctx.stroke();
+    ctx.draw();
+  },
+  // 暂停按钮 
+  pause:function(){
+    clearInterval(this.data.timer)
+    this.setData({
+      pauseShow:false,
+      continueCancelShow:true,     
+    })
+  },
+   continue:function(){
+     this.drawActive()
+     this.setData({
+       pauseShow:true,
+       continueCancelShow:false
+     })
+   },
+  //  放弃按钮 ---与完成按钮功能相同
+  cancel:function(){
+    clearInterval(this.data.timer);
+    this.setData({
+      pauseShow:true,
+      continueCancelShow:false,  
+      clockShow:false,
+      mTime:0   //白圈走的路清零
+    })
+  }
+})
---
+++ pages/logs/logs.js
@@ -0,0 +1,104 @@
+//logs.js
+const util = require('../../utils/util.js')
+
+Page({
+  data: {
+    logs: [],
+    dayList: [],  //今日
+    list: [],     //累计
+    actionIndex: 0,
+    sum: [{
+        title: '今日番茄次数',
+        val: '0'
+      },
+      {
+        title: '累计番茄次数',
+        val: '0'
+      },
+      {
+        title: '今日专注时长',
+        val: '0分钟'
+      },
+      {
+        title: '累计专注时长',
+        val: '0分钟'
+      }
+    ],
+    cateArr: [{
+      icon: 'work',
+      text: '工作'
+    }, {
+      icon: 'study',
+      text: '学习'
+    }, {
+      icon: 'think',
+      text: '思考'
+    }, {
+      icon: 'write',
+      text: '写作'
+    }, {
+      icon: 'sport',
+      text: '运动'
+    }, {
+      icon: 'read',
+      text: '阅读'
+    }]
+  },
+  onShow: function () {
+    this.setData({
+      logs: (wx.getStorageSync('logs') || []).map(log => {
+        return util.formatTime(new Date(log))
+      })
+    })
+    var logs = wx.getStorageSync('logs')||[];
+    // 为什么logs里面会有一串数字，哪里来的？
+    logs.pop();
+    console.log(logs)
+    var day = 0; //今日番茄次数
+    var total = logs.length; //累计番茄次数
+    var dayTime = 0; //今日时长
+    var totalTime = 0; //累计时长
+    var dayList=[];
+
+    if (logs.length > 0) {
+      for (var i = 0; i < logs.length; i++) {
+        if ((logs[i].date!==undefined) && logs[i].date.substr(0, 10) == util.formatTime(new Date).substr(0, 10)) {
+          // if(logs[i].date==util.formatTime(new Date)){
+          // console.log(0)
+          day = day + 1;
+          dayTime = dayTime + parseInt(logs[i].time)
+          dayList.push(logs[i])
+          this.setData({
+            dayList: dayList,
+            list: dayList
+          })
+        }
+        if (logs[i].date!==undefined){
+          totalTime = totalTime + parseInt(logs[i].time)
+        }  
+      };
+      this.setData({
+        'sum[0].val': day,
+        'sum[1].val': total,
+        'sum[2].val': dayTime ,
+        'sum[3].val': totalTime 
+      })
+    }
+  },
+  changeType: function (e) { 
+    var index = e.currentTarget.dataset.index
+    if (index == 0) { //今日
+      this.setData({
+        list: this.data.dayList
+      })
+    } else {         //历史
+      var logs = wx.getStorageSync('logs') || [];
+      this.setData({
+        list: logs
+      })
+    };
+    this.setData({
+      actionIndex: index
+    })
+  }
+})
